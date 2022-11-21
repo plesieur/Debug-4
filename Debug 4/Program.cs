f9e2656cee3ad17276a2cbfd18ce139f5267048e
Paul Lesieur <plesieur@jpkeefehs.org>
2022-11-21 09:30:08 -0500

    Add project files.

---
+++ Debug 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Debug_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digit = new int[2];
+            int index = 0;
+
+            Console.WriteLine("Hello World!");
+            Console.Write("Input two integers, seperated by a comma:");
+            string inputString = Console.ReadLine();
+            string[] InputToken = inputString.Split(',');
+            foreach (string token in InputToken)
+            {
+                digit[index] = Int32.Parse(token);
+            }
+            Console.WriteLine("{0}+{1}={2}", digit[0], digit[1], digit[0] + digit[1]);
+            Console.WriteLine("{0}-{1}={2}", digit[0], digit[1], digit[0] - digit[1]);
+            Console.WriteLine("{0}*{1}={2}", digit[0], digit[1], digit[0] * digit[1]);
+            Console.WriteLine("{0}/{1}={2}", digit[0], digit[1], digit[0] / digit[1]);
+        }
+    }
+}
